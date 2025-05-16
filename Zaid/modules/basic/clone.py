1a0a26f767a0d8b50e85b2556b0697f17c75faa0
SHASHANK <primiumshukla474@gmail.com>
2025-05-16 20:17:24 +0530

    Update clone.py

--- Zaid/modules/basic/clone.py
+++ Zaid/modules/basic/clone.py
@@ -1,81 +1,95 @@
-import os, sys, io, random
+import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from Zaid.helper.basic import edit_or_reply, get_text, get_user
 from Zaid.modules.help import add_command_help
+from Zaid.helper.functions import user_only, user_errors, delete_reply  # Ensure these are correctly implemented
 
+# Temporary memory-based profile store
 FName = ""
 LName = ""
 Bio = ""
 
 @Client.on_message(filters.command("clone", ".") & filters.me)
-async def clone_user(Shashank: Client, message: Message):
-    global FName 
-    global LName
-    global Bio
+async def clone_user(client: Client, message: Message):
+    global FName, LName, Bio
+
     try:
-       user = await user_only(Shashank, message)
-       if not user:
-          return 
+        user = await user_only(client, message)
+        if not user:
+            return
     except Exception as er:
-       await message.reply(user_errors(er))
-       return
-    Mai = await Shashank.get_me()
-    FName = Mai.first_name
-    if Mai.last_name:
-       LName = Mai.last_name
-    siu = await Shashank.get_chat("me")
-    if siu.bio:
-       Bio = siu.bio
-    Reply = await message.reply("cloning...")
-    _bio = await Shashank.get_chat(user.id)
-    if _bio.bio:
-       user_bio = _bio.bio
-    else:
-       user_bio = None
-    pic = await Shashank.download_media(user.photo.big_file_id)
+        await message.reply(user_errors(er))
+        return
+
+    # Store current profile
+    me = await client.get_me()
+    FName = me.first_name
+    LName = me.last_name if me.last_name else ""
+    me_chat = await client.get_chat("me")
+    Bio = me_chat.bio if me_chat.bio else ""
+
+    reply_msg = await message.reply("Cloning...")
+
+    # Get target user's bio
+    user_chat = await client.get_chat(user.id)
+    user_bio = user_chat.bio if user_chat.bio else None
+
+    # Try to download profile photo
+    pic = None
+    if user.photo:
+        try:
+            pic = await client.download_media(user.photo.big_file_id)
+        except Exception as e:
+            await reply_msg.edit(f"Failed to download photo: {e}")
+
     try:
-       await Shashank.set_profile_photo(photo=pic)
-       if user.last_name:
-          await Shashank.update_profile(first_name=user.first_name, last_name=user.last_name, bio=user_bio)
-       else:
-          await Shashank.update_profile(first_name=user.first_name, bio=user_bio)
-       await delete_reply(message, Reply, f"Now I'm {user.first_name} \n\n Note: Don't restart until you revert me!")
-    except Exception as eror:
-       await delete_reply(message, Reply, str(eror))
+        if pic:
+            await client.set_profile_photo(photo=pic)
+        if user.last_name:
+            await client.update_profile(first_name=user.first_name, last_name=user.last_name, bio=user_bio)
+        else:
+            await client.update_profile(first_name=user.first_name, bio=user_bio)
+
+        await delete_reply(message, reply_msg, f"Now I'm {user.first_name}!\n\nNote: Don't restart until you revert me!")
+    except Exception as error:
+        await delete_reply(message, reply_msg, str(error))
 
 @Client.on_message(filters.command("revert", ".") & filters.me)
-async def _revert(Shashank: Client, message: Message):
-    global FName 
-    global LName
-    global Bio
-    Mai = await Shashank.get_me()
-    photos = [x async for x in Shashank.get_chat_photos("me")]
-    if not FName:
-       await message.reply(f"Error: You didn't cloned anyone!")
-       return
-    user_bio = Bio
-    if not user_bio:
-       user_bio = "💕ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ sᴛʀᴀɴɢᴇʀ💕"
-    Reply = await message.reply("reverting...")
-    try:
-       if LName:
-          await Shashank.update_profile(first_name=FName, last_name=LName, bio=user_bio)
-       else:
-          await Shashank.update_profile(first_name=FName, bio=user_bio)
-       await Shashank.delete_profile_photos(photos[0].file_id)
-       await delete_reply(message, Reply, f"I'm Back!")
-       FName = ""
-       LName = ""
-       Bio = ""
-    except Exception as eror:
-       await delete_reply(message, Reply, str(eror))
+async def _revert(client: Client, message: Message):
+    global FName, LName, Bio
 
+    if not FName:
+        await message.reply("Error: You haven't cloned anyone!")
+        return
+
+    reply_msg = await message.reply("Reverting...")
+    user_bio = Bio or "💕ɪ ᴀᴍ ᴘᴀʀᴛ ᴏғ sᴛʀᴀɴɢᴇʀ💕"
+
+    try:
+        if LName:
+            await client.update_profile(first_name=FName, last_name=LName, bio=user_bio)
+        else:
+            await client.update_profile(first_name=FName, bio=user_bio)
+
+        photos = [x async for x in client.get_chat_photos("me")]
+        if photos:
+            await client.delete_profile_photos(photos[0].file_id)
+
+        await delete_reply(message, reply_msg, "I'm back!")
+
+        # Clear memory
+        FName = ""
+        LName = ""
+        Bio = ""
+
+    except Exception as error:
+        await delete_reply(message, reply_msg, str(error))
 
 add_command_help(
     "clone",
     [
         ["clone", "Clone someone’s profile (name, bio, photo). Reply to a user or use `.clone <username>`."],
-        ["revert", "Restore your original profile (name, bio, photo) using data saved in MongoDB."],
+        ["revert", "Restore your original profile (name, bio, photo)."],
     ],
 )
