e2af9ae79366df3fd48d81f2305e16f0555df84d
SHASHANK <primiumshukla474@gmail.com>
2025-05-17 06:05:49 +0530

    Update translate.py

--- Zaid/modules/basic/translate.py
+++ Zaid/modules/basic/translate.py
@@ -1,71 +1,69 @@
-from Zaid.modules.help import *
 import os
-from pyrogram import filters, Client
+from pyrogram import Client, filters
 from pyrogram.types import Message
 from py_trans import Async_PyTranslator
-from Zaid.helper.utility import get_arg
 
+from Zaid.modules.help import add_command_help
+from Zaid.helper.utility import get_arg
 
 
 @Client.on_message(filters.command(["tr", "translate"], ["."]) & filters.me)
 async def pytrans_tr(_, message: Message):
-  tr_msg = await message.edit("`Processing...`")
-  r_msg = message.reply_to_message
-  args = get_arg(message)
-  if r_msg:
-    if r_msg.text:
-      to_tr = r_msg.text
-    else:
-      return await tr_msg.edit("`Reply to a message that contains text!`")
-    # Checks if dest lang is defined by the user
-    if not args:
-      return await tr_msg.edit(f"`Please define a destination language!` \n\n**Ex:** `{Config.CMD_PREFIX}ptr si Hey, I'm using telegram!`")
-    # Setting translation if provided
-    else:
-      sp_args = args.split(" ")
-      if len(sp_args) == 2:
+    tr_msg = await message.edit("`Processing...`")
+    r_msg = message.reply_to_message
+    args = get_arg(message)
+
+    if not args and not r_msg:
+        return await tr_msg.edit("`Please reply to a message or provide text to translate.`")
+
+    if r_msg and r_msg.text:
+        to_tr = r_msg.text
+        if not args:
+            return await tr_msg.edit("`Please define a destination language.`\n\nExample: `.tr hi`")
+        sp_args = args.split(" ", 1)
         dest_lang = sp_args[0]
-        tr_engine = sp_args[1]
-      else:
-        dest_lang = sp_args[0]
-        tr_engine = "google"
-  elif args:
-    # Splitting provided arguments in to a list
-    a_conts = args.split(None, 2)
-    # Checks if translation engine is defined by the user
-    if len(a_conts) == 3:
-      dest_lang = a_conts[0]
-      tr_engine = a_conts[1]
-      to_tr = a_conts[2]
-    else:
-      dest_lang = a_conts[0]
-      to_tr = a_conts[1]
-      tr_engine = "google"
-  # Translate the text
-  py_trans = Async_PyTranslator(provider=tr_engine)
-  translation = await py_trans.translate(to_tr, dest_lang)
-  # Parse the translation message
-  if translation["status"] == "success":
-    tred_txt = f"""
+        tr_engine = sp_args[1] if len(sp_args) == 2 else "google"
+
+    elif args:
+        sp_args = args.split(" ", 2)
+        if len(sp_args) == 3:
+            dest_lang, tr_engine, to_tr = sp_args
+        elif len(sp_args) == 2:
+            dest_lang, to_tr = sp_args
+            tr_engine = "google"
+        else:
+            return await tr_msg.edit("`Please provide both language and text to translate.`\n\nExample: `.tr hi Hello World`")
+
+    try:
+        py_trans = Async_PyTranslator(provider=tr_engine)
+        translation = await py_trans.translate(to_tr, dest_lang)
+    except Exception as e:
+        return await tr_msg.edit(f"`Translation failed:` {e}")
+
+    if translation.get("status") == "success":
+        tred_txt = f"""
 **Translation Engine**: `{translation["engine"]}`
 **Translated to:** `{translation["dest_lang"]}`
 **Translation:**
 `{translation["translation"]}`
 """
-    if len(tred_txt) > 4096:
-      await tr_msg.edit("`Wah!! Translated Text So Long Tho!, Give me a minute, I'm sending it as a file!`")
-      tr_txt_file = open("translated.txt", "w+")
-      tr_txt_file.write(tred_txt)
-      tr_txt_file.close()
-      await tr_msg.reply_document("ptranslated_NEXAUB.txt")
-      os.remove("ptranslated.txt")
-      await tr_msg.delete()
+        if len(tred_txt) > 4096:
+            temp_file = "translated.txt"
+            with open(temp_file, "w", encoding="utf-8") as f:
+                f.write(tred_txt)
+            await tr_msg.reply_document(temp_file, caption="`Translated Text File`")
+            os.remove(temp_file)
+            await tr_msg.delete()
+        else:
+            await tr_msg.edit(tred_txt)
     else:
-      await tr_msg.edit(tred_txt)
+        await tr_msg.edit("`Failed to translate the text.`")
+
 
 add_command_help(
     "translate",
     [
-        [".tr", "Translate some text by give a text or reply that text/caption."],
+        [".tr [lang] [text]", "Translate the given text to the target language."],
+        [".tr [lang]", "Reply to a message to translate it to the specified language."],
     ],
 )
