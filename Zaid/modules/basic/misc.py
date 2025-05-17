6e8976dbbe89654699e5dc724bf81f3a5e18dae6
SHASHANK <primiumshukla474@gmail.com>
2025-05-17 07:26:35 +0530

    Update misc.py

--- Zaid/modules/basic/misc.py
+++ Zaid/modules/basic/misc.py
@@ -62,14 +62,14 @@ async def repo(bot: Client, message: Message):
 
 @Client.on_message(filters.command("creator", ".") & filters.me)
 async def creator(bot: Client, message: Message):
-    await message.edit("https://t.me/SHIVANSH474")
+    await message.edit("@SHIVANSHDEVS")
 
 
 @Client.on_message(filters.command(["uptime", "up"], ".") & filters.me)
 async def uptime(bot: Client, message: Message):
     now = datetime.now()
     current_uptime = now - START_TIME
-    await message.edit(f"Uptime ⚡\n" f"```{str(current_uptime).split('.')[0]}```")
+    await message.edit(f"ᴜᴘᴛɪᴍᴇ ⚡\n" f"```{str(current_uptime).split('.')[0]}```")
 
 
 @Client.on_message(filters.command("id", ".") & filters.me)
@@ -81,45 +81,45 @@ async def get_id(bot: Client, message: Message):
         rep = message.reply_to_message
 
         if rep.audio:
-            file_id = f"**File ID**: `{rep.audio.file_id}`"
-            file_id += "**File Type**: `audio`"
+            file_id = f"**ғɪʟᴇ ɪᴅ**: `{rep.audio.file_id}`"
+            file_id += "**ғɪʟᴇ ᴛʏᴘᴇ**: `audio`"
 
         elif rep.document:
-            file_id = f"**File ID**: `{rep.document.file_id}`"
-            file_id += f"**File Type**: `{rep.document.mime_type}`"
+            file_id = f"**ғɪʟᴇ ɪᴅ**: `{rep.document.file_id}`"
+            file_id += f"**ғɪʟᴇ ᴛʏᴘᴇ**: `{rep.document.mime_type}`"
 
         elif rep.photo:
-            file_id = f"**File ID**: `{rep.photo.file_id}`"
-            file_id += "**File Type**: `photo`"
+            file_id = f"**ғɪʟᴇ ɪᴅ**: `{rep.photo.file_id}`"
+            file_id += "**ғɪʟᴇ ᴛʏᴘᴇ**: `photo`"
 
         elif rep.sticker:
-            file_id = f"**Sicker ID**: `{rep.sticker.file_id}`\n"
+            file_id = f"**sɪᴄᴋᴇʀ ɪᴅ**: `{rep.sticker.file_id}`\n"
             if rep.sticker.set_name and rep.sticker.emoji:
-                file_id += f"**Sticker Set**: `{rep.sticker.set_name}`\n"
-                file_id += f"**Sticker Emoji**: `{rep.sticker.emoji}`\n"
+                file_id += f"**sᴛɪᴄᴋᴇʀ sᴇᴛ**: `{rep.sticker.set_name}`\n"
+                file_id += f"**sᴛɪᴄᴋᴇʀ ᴇᴍᴏᴊɪ**: `{rep.sticker.emoji}`\n"
                 if rep.sticker.is_animated:
-                    file_id += f"**Animated Sticker**: `{rep.sticker.is_animated}`\n"
+                    file_id += f"**ᴀɴɪᴍᴀᴛᴇᴅ sᴛɪᴄᴋᴇʀ**: `{rep.sticker.is_animated}`\n"
                 else:
-                    file_id += "**Animated Sticker**: `False`\n"
+                    file_id += "**ᴀɴɪᴍᴀᴛᴇᴅ sᴛɪᴄᴋᴇʀ**: `False`\n"
             else:
-                file_id += "**Sticker Set**: __None__\n"
-                file_id += "**Sticker Emoji**: __None__"
+                file_id += "**sᴛɪᴄᴋᴇʀ sᴇᴛ**: __None__\n"
+                file_id += "**sᴛɪᴄᴋᴇʀ ᴇᴍᴏᴊɪ**: __None__"
 
         elif rep.video:
-            file_id = f"**File ID**: `{rep.video.file_id}`\n"
-            file_id += "**File Type**: `video`"
+            file_id = f"**ғɪʟᴇ ɪᴅ**: `{rep.video.file_id}`\n"
+            file_id += "**ғɪʟᴇ ᴛʏᴘᴇ**: `video`"
 
         elif rep.animation:
-            file_id = f"**File ID**: `{rep.animation.file_id}`\n"
-            file_id += "**File Type**: `GIF`"
+            file_id = f"**ғɪʟᴇ ɪᴅ**: `{rep.animation.file_id}`\n"
+            file_id += "**ғɪʟᴇ ᴛʏᴘᴇ**: `GIF`"
 
         elif rep.voice:
-            file_id = f"**File ID**: `{rep.voice.file_id}`\n"
-            file_id += "**File Type**: `Voice Note`"
+            file_id = f"**ғɪʟᴇ ɪᴅ**: `{rep.voice.file_id}`\n"
+            file_id += "**ғɪʟᴇ ᴛʏᴘᴇ**: `Voice Note`"
 
         elif rep.video_note:
-            file_id = f"**File ID**: `{rep.animation.file_id}`\n"
-            file_id += "**File Type**: `Video Note`"
+            file_id = f"**ғɪʟᴇ ɪᴅ**: `{rep.animation.file_id}`\n"
+            file_id += "**ғɪʟᴇ ᴛʏᴘᴇ**: `Video Note`"
 
         elif rep.location:
             file_id = "**Location**:\n"
