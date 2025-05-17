48c21ad317683bee9ccb107c7c8e81381aeaef7b
SHASHANK <primiumshukla474@gmail.com>
2025-05-17 07:10:35 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -25,7 +25,7 @@ async def module_help(client: Client, message: Message):
     if len(cmd) > 1:
         help_arg = " ".join(cmd[1:])
     elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("⚡️")
+        await message.edit("💞")
         try:
             nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
             await asyncio.gather(
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "⚡️𝗦𝗧𝗥𝗔𝗡𝗚𝗘𝗥⚡️"
+            ac.title = "❁ sᴛʀᴀɴɢᴇʀ-ʙᴏᴛ ᴘʟᴜɢɪɴs ❁"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @SHIVANSH474 × @MASTIWITHFRIENDSXD •",
+                f"```{str(ac)}```\n• @SHIVANSH474 × @SHIVANSHDEVS •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @SHIVANSH474 × @MASTIWITHFRIENDSXD •"
+            message, f"```{str(ac)}```\n• @SHIVANSH474 × @SHIVANSHDEVS •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
