fc19cec3f516ff642584bfe84ffa9def791a0965
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-05-18 17:07:18 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -38,7 +38,7 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "❁ sᴛʀᴀɴɢᴇʀ-ʙᴏᴛ ᴘʟᴜɢɪɴs ❁"
+            ac.title = "❁ 𝙎𝙏𝚲𝙇𝙆𝙀𝙍-ʙᴏᴛ ᴘʟᴜɢɪɴs ❁"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
