ca825b25e1eeb52b97ec4238e8195d0714783a5a
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-05-23 12:55:39 +0530

    Update Shayari.py

--- Zaid/modules/basic/Shayari.py
+++ Zaid/modules/basic/Shayari.py
@@ -141,8 +141,8 @@ async def mentionall(client, message):
 
         if usrnum == 1:
             if mode == "text_on_cmd":
-                txt = f"{usrtxt} {reply}"
                 reply = choice(SHAYRI)
+                txt = f"{usrtxt} {reply}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(f"[{random.choice(EMOJI)}](tg://user?id={usr.user.id})")
